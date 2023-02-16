13a535a50d8d20cc56d43d5d2d5bf512eb55dcdc
actiangent <hisyam.fausta@gmail.com>
2023-02-16 22:06:27 +0700

    add unit and instrumentation test folder, create LocalDateTimeUtil unit test

---
+++ app/src/test/java/com/actiangent/note/util/LocalDateTimeUtilTest.kt
@@ -0,0 +1,32 @@
+package com.actiangent.note.util
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+
+class LocalDateTimeUtilTest {
+
+    private val dateTimePatternFormat = "yyyy-MM-dd HH:mm"
+
+    @Test
+    fun dateTimeFormat_verifyTrue() {
+        val todayDateTime = todayDateTimeFormatted()
+
+        assertTrue(isDateTimeValid(dateTimePatternFormat, todayDateTime))
+    }
+
+    private fun isDateTimeValid(format: String, dateTime: String): Boolean {
+        return try {
+            LocalDateTime.parse(
+                dateTime,
+                DateTimeFormatter.ofPattern(format)
+            )
+            true
+        } catch (exception: DateTimeParseException) {
+            false
+        }
+    }
+
+}
