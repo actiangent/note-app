651f5f13777485dbdac7fa32449a705027e51ce4
actiangent <hisyam.fausta@gmail.com>
2023-02-16 21:33:29 +0700

    remove setItems() binding for NotListAdapter

--- app/src/main/java/com/actiangent/note/ui/fragment/home/HomeBindingAdapter.kt
+++ app/src/main/java/com/actiangent/note/ui/fragment/home/HomeBindingAdapter.kt
@@ -1,23 +1,9 @@
 package com.actiangent.note.ui.fragment.home
 
-import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.core.widget.doOnTextChanged
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.ConcatAdapter
-import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Query
-import com.actiangent.note.data.model.Note
-
-@BindingAdapter("app:items")
-fun setItems(recyclerView: RecyclerView, items: List<Note>?) {
-    val homeNoteListAdapter =
-        ((recyclerView.adapter as ConcatAdapter).adapters[1] as HomeNoteListAdapter)
-    items?.let {
-        homeNoteListAdapter.submitList(items)
-    }
-}
 
 @BindingAdapter("app:setOnQueryChanged")
 fun setOnQueryChanged(editText: EditText, viewModel: HomeViewModel) =
